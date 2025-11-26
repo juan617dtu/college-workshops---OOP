877fc0fa905c3061b22f25c199e3e10f908fa0fe
Juan Cuéllar <96099514+juan617dtu@users.noreply.github.com>
2025-11-26 10:33:22 -0500

    Add files via upload

---
+++ WS_10/dynaCopy.h
@@ -0,0 +1,35 @@
+//
+// Created by masti on 11/25/2025.
+//
+
+#ifndef DYNACOPY_H
+#define DYNACOPY_H
+
+template <typename T>
+T* dynaCopy(const T* array, int size) {
+    if (array == nullptr || size <= 0) return nullptr; //nothing to copy
+    T* result = new T[size];
+    for (size_t i = 0; i < size; ++i) result[i] = array[i];
+    return result;
+}
+template <typename T>
+T* dynaCopy(T*& dest_array, const T* src_array, int size) {
+    if (src_array == nullptr || size <= 0) {
+        delete[] dest_array;
+        dest_array = nullptr;
+        return nullptr;
+    }
+    delete[] dest_array;
+    dest_array = new T[size];
+    for (size_t i = 0; i < size; ++i) dest_array[i] = src_array[i];
+    return dest_array;
+}
+template <typename T>
+void prnArray(const T* array, int size) {
+    for (size_t i = 1; i <= size; ++i) {
+        if (i == size) std::cout << array[i - 1];
+        else { std::cout << array[i - 1] << ", "; }
+    }
+    std::cout << std::endl;
+}
+#endif //DYNACOPY_H
