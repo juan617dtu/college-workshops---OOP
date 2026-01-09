293c9f1ee2823b160f57034f1db6ee262f5fc32f
Juan Cuéllar <96099514+juan617dtu@users.noreply.github.com>
2026-01-09 12:50:26 -0500

    Add files via upload

---
+++ PROJECT/milestone4/Ordering.h
@@ -0,0 +1,42 @@
+//
+// Created by masti on 1/8/2026.
+//
+
+#ifndef ORDERING_H
+#define ORDERING_H
+#include "Food.h"
+#include "Drink.h"
+#include "Billable.h"
+#include "constants.h"
+
+namespace seneca {
+
+    class Ordering {
+
+        unsigned int m_food_counter{};
+        unsigned int m_drink_counter{};
+        unsigned int m_billable_counter{};
+        size_t m_bill_sn{ 1 };
+        Food* m_food_arr{};
+        Drink* m_drink_arr{};
+        Billable* m_bill_items[MaximumNumberOfMenuItems]{};
+        std::ostream& bill_title_print(std::ostream& ostr) const;
+        std::ostream& print_totals(std::ostream& ostr, double total_amount) const;
+        size_t countRecords(const char* file) const;
+
+    public:
+
+        Ordering(const char* drinks_file, const char* foods_file);
+        ~Ordering();
+        operator bool() const;
+        unsigned int noOfBillItems() const;
+        bool hasUnsavedBill() const;
+        void listFoods() const;
+        void listDrinks() const;
+        void orderFood();
+        void orderDrink();
+        std::ostream& printBill(std::ostream& ostr) const;
+        void resetBill();
+    };
+}
+#endif //ORDERING_H
---
+++ PROJECT/milestone4/Utils.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include "Utils.h"
+#include <limits>
+
+using namespace std;
+namespace seneca {
+    Utils ut;
+
+    char* Utils::alocpy(const char* src) const{
+        char* des{};
+        return alocpy(des, src);
+    }
+
+    char* Utils::alocpy(char*& des, const char* src)const {
+        delete[] des;
+        des = nullptr;
+        if (src) {
+            des = new char[strlen(src) + 1];
+            strcpy(des, src);
+        }
+        return des;
+    }
+    char* Utils::strcpy(char* des, const char* src)const {
+        int i;
+        for (i = 0; src[i]; i++) des[i] = src[i];
+        des[i] = char(0);
+        return des;
+    }
+    int Utils::strlen(const char* str)const {
+        int len;
+        for (len = 0; str[len]; len++);
+        return len;
+    }
+
+    bool Utils::isspace(char ch)const {
+        return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\v' || ch == '\f' || ch == '\r';
+    }
+
+    bool Utils::isspace(const char* cstring) const{
+        while (cstring && isspace(*cstring)) {
+            cstring++;
+        }
+        return cstring && *cstring == 0;
+    }
+    int Utils::getInt() {
+
+        int num;
+        bool valid = false;
+        do {
+            if (cin.peek() == '\n') cout << "You must enter a value: ";
+            else {
+                cin >> num;
+                if (cin.fail()) {
+                    cout << "Invalid integer: ";
+                }
+                else if (cin.peek() != '\n') cout << "Only an integer please: ";
+                else valid = true;
+            }
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        } while (!valid);
+        return num;
+    }
+    int Utils::getInt(int min, int max) {
+
+        int num;
+        bool valid = false;
+        do {
+            num = getInt();
+            if (num < min || num > max) cout << "Invalid value: [min <= value <= max], try again: ";
+            else valid = true;
+        } while (!valid);
+        return num;
+    }
+    char* Utils::makeBillFileName(char* filename, size_t billNo)const {
+        char billFileName[21] = "bill_";
+        size_t temp = billNo;
+        int cnt = 5;
+        int length;
+        // Calculate the number of digits
+        do {
+            cnt++;
+            temp /= 10;
+        } while (temp > 0);
+        length = cnt;
+        // Convert each digit to character from the end
+        while (billNo > 0) {
+            billFileName[--cnt] = (billNo % 10) + '0';
+            billNo /= 10;
+        }
+        // Handle the case when billNo is 0
+        if (billFileName[cnt - 1] == '\0') {
+            billFileName[--cnt] = '0';
+        }
+        // Attach .txt to the end of the file name
+        for (int i = 0; ".txt"[i]; i++) {
+            billFileName[length++] = ".txt"[i];
+        }
+        billFileName[length] = '\0';
+        strcpy(filename, billFileName);
+        return filename;
+    }
+}
